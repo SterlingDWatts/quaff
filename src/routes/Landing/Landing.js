e3a0675facb0e4b269e388c55ebbd5fc897ddcc0
Sterling Watts 🍷 <SterlingDWatts@gmail.com>
2020-12-27 13:57:02 -0800

    Change Landing to functional component

--- src/routes/Landing/Landing.js
+++ src/routes/Landing/Landing.js
@@ -1,40 +1,37 @@
-import React, { Component } from "react";
+import React, { useContext, useEffect } from "react";
 import { QuaffSquare } from "../../components/Utils/Utils";
 import NextUp from "../../components/NextUp/NextUp";
 import QuizListContext from "../../contexts/QuizListContext";
 import ModulesApiService from "../../services/modules-api-service";
 import "./Landing.css";
 
-class Landing extends Component {
-  static contextType = QuizListContext;
+const Landing = () => {
+  const context = useContext(QuizListContext);
 
-  componentDidMount() {
-    this.context.clearError();
+  useEffect(() => {
+    const { clearError, setQuizList, setTopicList, setError } = context;
+    clearError();
     ModulesApiService.getModules()
       .then((res) => {
-        this.context.setQuizList(res);
-        ModulesApiService.getTopics()
-          .then(this.context.setTopicList)
-          .catch(this.context.setError);
+        setQuizList(res);
+        ModulesApiService.getTopics().then(setTopicList).catch(setError);
       })
-      .catch(this.context.setError);
-  }
+      .catch(setError);
+  }, []);
 
-  render() {
-    return (
-      <div className="Landing">
-        <QuaffSquare>
-          <h1>
-            Quaff
-            <br />
-            Quizz
-          </h1>
-          <h4>For the inquisitive oenophile</h4>
-        </QuaffSquare>
-        <NextUp />
-      </div>
-    );
-  }
-}
+  return (
+    <div className="Landing">
+      <QuaffSquare>
+        <h1>
+          Quaff
+          <br />
+          Quizz
+        </h1>
+        <h4>For the inquisitive oenophile</h4>
+      </QuaffSquare>
+      <NextUp />
+    </div>
+  );
+};
 
 export default Landing;
